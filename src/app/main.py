ba3e5ab17dc9301ea45b3116a584b2251db25212
fatihalharitz <fatih.alharitz@binus.ac.id>
2025-04-29 22:48:43 +0700

    dev branch

--- src/app/main.py
+++ src/app/main.py
@@ -1,7 +1,5 @@
 from flask import Flask, render_template, jsonify
-from .scanner import run_scan
-from .parser import parse_scan_results
-
+from scanner.main_scanner import run_scan_with_progress
 import os
 import api.main_api as main_api
 import json
@@ -19,18 +17,11 @@ def home():
 def scan():
     app.logger.debug("scanning")
     try:
-        raw_results = run_scan()
-        app.logger.debug("parsing results")
+        scan_results = run_scan_with_progress()
         
-        parsed_results = parse_scan_results(raw_results)
-        
-        # return render_template('scan.html', results=parsed_results)
-
-        app.logger.debug("getting mitigation")
-        with open("scan_results.json", "w") as f:
-            json.dump(parsed_results, f, indent=2)
-        
-        mitigation_results = main_api.main(parsed_results)
+        # Convert dict to JSON string before passing to main_api
+        json_results = json.dumps(scan_results)
+        mitigation_results = main_api.main(json_results)
 
         app.logger.debug("done!")
         return render_template('result.html', results=mitigation_results)
@@ -50,4 +41,4 @@ def detail():
     return render_template('detail.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)         
+    app.run(debug=True)
